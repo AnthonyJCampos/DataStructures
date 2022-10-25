30eb660c7c18fcc7cd27c72c80e7f8ee676f2a58
Anthony Campos <Anthonyck89@hotmail.com>
2022-10-25 12:15:28 -0700

    remove in dev

--- Trees/Java/BTS.java
+++ Trees/Java/BTS.java
@@ -22,10 +22,20 @@ public class BTS<T extends Comparable<T>> {
    public BTS() {
    } // end defualt constructor
 
-   public void add(T value) {
+   public int length() {
+      return size;
+   } // end length()
 
+   public boolean add(T value) {
+
+      // store previous size to determine if add was successful
+      int prevSize = size;
       this.root = addHelper(this.root, value);
-   }
+      if (size > prevSize) {
+         return true;
+      } // end if
+      return false;
+   } // end add
 
    private Node addHelper(Node root, T value) {
 
@@ -37,14 +47,126 @@ public class BTS<T extends Comparable<T>> {
          return root;
       } // end if
 
-      if (root.value.compareTo(value) > 1) {
+      int check = root.value.compareTo(value);
+      if (check > 0) {
          root.left = addHelper(root.left, value);
-      } else if (root.value.compareTo(value) < 0) {
+      } else if (check < 0) {
          root.right = addHelper(root.right, value);
       } // end if
 
       return root;
 
    } // end addHelper
+
+   /** Work In Progress */
+   public boolean remove(T value) {
+
+      int tempSize = size;
+      root = removeHelper(root, value);
+      if (tempSize > size) {
+         return true;
+      } // end if
+      return false;
+   } // end remove
+
+   private Node removeHelper(Node root, T value) {
+
+      // base case, empty tree
+      if (root == null) {
+         return null;
+      } // case 1: value is equal to root value
+      else if (root.value.compareTo(value) == 0) {
+
+         // delete node
+         deleteNode(root);
+         size--;
+      } // case 2: value is less then root value
+      else if (root.value.compareTo(value) > 0) {
+         root = removeHelper(root.left, value);
+      } // case 3: value is greater then root value
+      else {
+         root = removeHelper(root.right, value);
+      } // end if
+
+      return root;
+
+   } // end removeHelper
+
+   private void deleteNode(Node root) {
+
+      // case 1: root/leaf
+      if (root.left == null && root.right == null) {
+
+         root = null;
+      }
+      // case 2: only one child
+      else if (root.left == null || root.right == null) {
+
+         // determine which child to delete
+         if (root.left == null) {
+            root = root.right;
+         } else {
+            root = root.left;
+         } // end if
+      }
+      // case 3 two children
+      else {
+
+         root.value = removeLeftMostNode(root.right);
+      } // end if
+
+   } // end deleteNode
+
+   private T removeLeftMostNode(Node root) {
+
+      if (root.left == null) {
+
+         root = root.right;
+
+      } else {
+         return removeLeftMostNode(root.left);
+      } // end if
+
+      return root.value;
+   } // end removeLeftMostNode
+
+   public void printInorder() {
+      inorder(this.root);
+   } // end printIn
+
+   private void inorder(Node root) {
+
+      if (root != null) {
+         inorder(root.left);
+         System.out.printf("%s ", root.value);
+         inorder(root.right);
+      } // end if
+
+   } // end inorder
+
+   public void printPreorder() {
+      preorder(this.root);
+   } // end printPreorder
+
+   private void preorder(Node root) {
+      if (root != null) {
+         System.out.printf("%s ", root.value);
+         preorder(root.left);
+         preorder(root.right);
+      } // end if
+   } // end preorder
+
+   public void printPostorder() {
+      postorder(this.root);
+   } // end printPostOrder()
+
+   private void postorder(Node root) {
+
+      if (root != null) {
+         postorder(root.left);
+         postorder(root.right);
+         System.out.printf("%s ", root.value);
+      } // end if
+   } // end postorder
 
 } // end of BTS
--- Trees/Java/Main.java
+++ Trees/Java/Main.java
@@ -1,5 +1,7 @@
 package Trees.Java;
 
+import java.util.*;
+
 public class Main {
 
    /**
@@ -9,10 +11,36 @@ public class Main {
 
       BTS<Integer> tree1 = new BTS<>();
 
-      tree1.add(1);
+      // int[] arrayOfTree = new int[15];
+      ArrayList<Integer> arrayOfTree = new ArrayList<>();
 
+      randamFillTreeNum(tree1, arrayOfTree);
+      // linearFillTreeNum(tree1);
+      tree1.printInorder();
+      System.out.println(tree1.length());
       System.out.println("Break");
 
    }
+
+   // build out tree
+
+   public static void randamFillTreeNum(BTS<Integer> tree, ArrayList<Integer> array) {
+
+      for (int i = 0; i < 15; i++) {
+         int rand = (int) Math.round(Math.random() * 1000 + 1);
+         System.out.println("current random num: " + rand);
+         if (tree.add(rand)) {
+            array.add(rand);
+         } // end if
+      } // end for
+
+   } // end randamFillTreeNum
+
+   public static void linearFillTreeNum(BTS<Integer> tree) {
+
+      for (int i = 0; i < 10; i++) {
+         tree.add(i);
+      } // end for
+   } // end linearFillTreeNum
 
 } // end main
